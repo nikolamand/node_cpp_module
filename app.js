e7fb1f9adb7f371b2b560eb0086105a55e73272e
nikolamand <nikolamand@gmail.com>
2018-07-23 12:30:44 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// hello.js
+const hello = require('./build/Release/hello');
+
+console.log(hello.hello());
+// Prints: 'world'
+
+
+
+const callback = require('./build/Release/callback');
+
+
+let testPromise = new Promise((resolve, reject) => {
+  callback((msg) => {
+    resolve(msg);
+  });
+  });
+  
+let functionWithPromise = () => {
+  
+
+  (async () => {
+    let users = await testPromise.then((successMessage) => {
+      console.log(successMessage);
+    });
+  })();
+  
+  
+  console.log(`Text after of promise`);
+}
+functionWithPromise();
+
+
+
+const cppObj = require('./build/Release/objects');
+const obj1 = cppObj('OBJECT 1');
+const obj2 = cppObj('Object 2');
+console.log(obj1.msg, obj2.msg);
+// Prints: 'hello world'
---
+++ binding.gyp
@@ -0,0 +1,17 @@
+{
+  "targets": [
+    {
+      "target_name": "hello",
+      "sources": [ "hello.cc" ]
+    },
+    {
+      "target_name": "objects",
+      "sources": [ "objects.cc" ]
+    },
+    {
+      "target_name": "callback",
+      "sources": [ "callback.cc" ]
+    }
+    
+  ]
+}
